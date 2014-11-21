55bb28cffff13180c82496e8747fe8609c764ebb
Hannes Widmoser <widmoser@gmail.com>
2014-11-21 18:56:54 +0100

    simplified document browser interfaces

--- library/src/main/java/com/mobilejazz/coltrane/library/DocumentBrowser.java
+++ library/src/main/java/com/mobilejazz/coltrane/library/DocumentBrowser.java
@@ -1,6 +1,8 @@
 package com.mobilejazz.coltrane.library;
 
+import android.content.res.AssetFileDescriptor;
 import android.database.Cursor;
+import android.graphics.Point;
 import android.os.CancellationSignal;
 import android.os.ParcelFileDescriptor;
 
@@ -15,5 +17,17 @@ public interface DocumentBrowser {
     public Cursor queryDocument(String documentId, String[] projection) throws FileNotFoundException;
 
     public ParcelFileDescriptor openDocument(String documentId, String mode, CancellationSignal signal) throws FileNotFoundException;
+
+    public AssetFileDescriptor openDocumentThumbnail(final String documentId, final Point sizeHint, final CancellationSignal signal) throws FileNotFoundException;
+
+    public String createDocument(String parentDocumentId, String mimeType, String displayName) throws FileNotFoundException;
+
+    public String renameDocument(String documentId, String displayName) throws FileNotFoundException;
+
+    public void deleteDocument(String documentId) throws FileNotFoundException;
+
+    public Cursor queryRecentDocuments(String rootId, String[] projection) throws FileNotFoundException;
+
+    public Cursor querySearchDocuments(String rootId, String query, String[] projection) throws FileNotFoundException;
 
 }
--- library/src/main/java/com/mobilejazz/coltrane/library/DocumentCreator.java
+++
@@ -1,9 +0,0 @@
-package com.mobilejazz.coltrane.library;
-
-import java.io.FileNotFoundException;
-
-public interface DocumentCreator {
-
-    public String createDocument(String parentDocumentId, String mimeType, String displayName) throws FileNotFoundException;
-
-}
