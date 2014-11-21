740144b336ffa5bbd3f2f2c056e4c4a32c9b9460
Hannes Widmoser <widmoser@gmail.com>
2014-11-21 19:09:39 +0100

    added a SAF documents adapter for file system

--- library/src/main/java/com/mobilejazz/coltrane/library/DocumentsProviderAdapter.java
+++ library/src/main/java/com/mobilejazz/coltrane/library/DocumentsProviderAdapter.java
@@ -10,62 +10,58 @@ import android.os.ParcelFileDescriptor;
 import java.io.FileNotFoundException;
 
 @TargetApi(19)
-public class DocumentsProviderAdapter extends android.provider.DocumentsProvider {
+public abstract class DocumentsProviderAdapter extends android.provider.DocumentsProvider {
 
-    private DocumentsProvider mDelegate;
-
-    public DocumentsProviderAdapter(DocumentsProvider delegate) {
-        this.mDelegate = delegate;
-    }
+    protected abstract DocumentsProvider getDelegate();
 
     @Override
     public Cursor queryRoots(String[] projection) throws FileNotFoundException {
-        return mDelegate.queryRoots(projection);
+        return getDelegate().queryRoots(projection);
     }
 
     @Override
     public Cursor queryDocument(String documentId, String[] projection) throws FileNotFoundException {
-        return mDelegate.queryDocument(documentId, projection);
+        return getDelegate().queryDocument(documentId, projection);
     }
 
     @Override
     public Cursor queryChildDocuments(String parentDocumentId, String[] projection, String sortOrder) throws FileNotFoundException {
-        return mDelegate.queryChildDocuments(parentDocumentId, projection, sortOrder);
+        return getDelegate().queryChildDocuments(parentDocumentId, projection, sortOrder);
     }
 
     @Override
     public ParcelFileDescriptor openDocument(String documentId, String mode, CancellationSignal signal) throws FileNotFoundException {
-        return mDelegate.openDocument(documentId, mode, signal);
+        return getDelegate().openDocument(documentId, mode, signal);
     }
 
     @Override
     public AssetFileDescriptor openDocumentThumbnail(String documentId, Point sizeHint, CancellationSignal signal) throws FileNotFoundException {
-        return mDelegate.openDocumentThumbnail(documentId, sizeHint, signal);
+        return getDelegate().openDocumentThumbnail(documentId, sizeHint, signal);
     }
 
     @Override
     public Cursor queryRecentDocuments(String rootId, String[] projection) throws FileNotFoundException {
-        return mDelegate.queryRecentDocuments(rootId, projection);
+        return getDelegate().queryRecentDocuments(rootId, projection);
     }
 
     @Override
     public Cursor querySearchDocuments(String rootId, String query, String[] projection) throws FileNotFoundException {
-        return mDelegate.querySearchDocuments(rootId, query, projection);
+        return getDelegate().querySearchDocuments(rootId, query, projection);
     }
 
     @Override
     public String createDocument(String parentDocumentId, String mimeType, String displayName) throws FileNotFoundException {
-        return mDelegate.createDocument(parentDocumentId, mimeType, displayName);
+        return getDelegate().createDocument(parentDocumentId, mimeType, displayName);
     }
 
     @Override
     public String renameDocument(String documentId, String displayName) throws FileNotFoundException {
-        return mDelegate.renameDocument(documentId, displayName);
+        return getDelegate().renameDocument(documentId, displayName);
     }
 
     @Override
     public void deleteDocument(String documentId) throws FileNotFoundException {
-        mDelegate.deleteDocument(documentId);
+        getDelegate().deleteDocument(documentId);
     }
 
     @Override
