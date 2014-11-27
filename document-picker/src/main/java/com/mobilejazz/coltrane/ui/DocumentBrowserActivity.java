49dd209b6dc3b660d146d5c3d745fc726eafa54a
Hannes Widmoser <widmoser@gmail.com>
2014-11-27 12:20:54 +0100

    added a sliding drawer to select providers

--- document-picker/src/main/java/com/mobilejazz/coltrane/ui/DocumentBrowserActivity.java
+++ document-picker/src/main/java/com/mobilejazz/coltrane/ui/DocumentBrowserActivity.java
@@ -24,10 +24,18 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.provider.DocumentsContract;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.mobilejazz.coltrane.library.DocumentsProvider;
@@ -36,6 +44,8 @@ import com.mobilejazz.coltrane.library.utils.DocumentCursor;
 import com.mobilejazz.coltrane.library.utils.RootCursor;
 
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Main Activity that handles the FileListFragments
@@ -43,8 +53,9 @@ import java.io.FileNotFoundException;
 public class DocumentBrowserActivity extends Activity implements
         FragmentManager.OnBackStackChangedListener, DocumentListFragment.Callbacks {
 
-    public static final String PROVIDER = "provider";
-    public static final String PATH = "path";
+    public static final String PROVIDER = "com.mobilejazz.coltrane.ui.browser.provider";
+    public static final String PATH = "com.mobilejazz.coltrane.ui.browser.path";
+    public static final String SELECTED_ITEM = "com.mobilejazz.coltrane.ui.browser.selected";
 
     public static final String RESULT_ID = DocumentsContract.Document.COLUMN_DOCUMENT_ID;
 
@@ -61,32 +72,60 @@ public class DocumentBrowserActivity extends Activity implements
     private String mCurrentDocumentId;
     private String mRootId;
 
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+
+    private ArrayAdapter<DocumentsProvider> mDrawerAdapter;
+    private Map<DocumentsProvider, Integer> mProviderIndices;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.picker);
+
+        // Setting up the navigation:
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerAdapter = new ArrayAdapter<DocumentsProvider>(this, R.layout.provider, DocumentsProviderRegistry.get().getAll().toArray(new DocumentsProvider[] {}));
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        mDrawerList.setAdapter(mDrawerAdapter);
+        populateProviderIndices();
+
+        getActionBar().setDisplayShowHomeEnabled(false);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
         mFragmentManager = getFragmentManager();
         mFragmentManager.addOnBackStackChangedListener(this);
 
-        mProvider = DocumentsProviderRegistry.get().getAll().iterator().next(); // TODO: make this more general
-        try {
-            RootCursor c = new RootCursor(mProvider.queryRoots(null));
-            c.moveToFirst();
-            mRootId = c.getDocumentId();
-            if (savedInstanceState == null) {
-                mCurrentDocumentId = mRootId;
-                addFragment();
-            } else {
-                mCurrentDocumentId = savedInstanceState.getString(PATH);
-            }
-            setTitle(c.getTitle());
-            c.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            finish();
+        final DocumentsProvider provider;
+        if (savedInstanceState == null) {
+            provider = DocumentsProviderRegistry.get().getDefault();
+            replaceFragment(provider, null);
+            mDrawerList.setItemChecked(mProviderIndices.get(provider), true);
+        } else {
+            provider = DocumentsProviderRegistry.get().getProvider(savedInstanceState.getString(PROVIDER));
+            changeProvider(provider, savedInstanceState.getString(PATH));
+            mDrawerList.setItemChecked(savedInstanceState.getInt(SELECTED_ITEM), true);
         }
-
-
     }
 
     @Override
@@ -108,6 +147,8 @@ public class DocumentBrowserActivity extends Activity implements
         super.onSaveInstanceState(outState);
 
         outState.putString(PATH, mCurrentDocumentId);
+        outState.putString(PROVIDER, mProvider.getId());
+        outState.putInt(SELECTED_ITEM, mDrawerList.getSelectedItemPosition());
     }
 
     @Override
@@ -126,18 +167,34 @@ public class DocumentBrowserActivity extends Activity implements
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        boolean hasBackStack = mFragmentManager.getBackStackEntryCount() > 0;
+        //boolean hasBackStack = mFragmentManager.getBackStackEntryCount() > 0;
 
-        ActionBar actionBar = getActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(hasBackStack);
-        actionBar.setHomeButtonEnabled(hasBackStack);
+        //ActionBar actionBar = getActionBar();
+        //actionBar.setDisplayHomeAsUpEnabled(hasBackStack);
+        //actionBar.setHomeButtonEnabled(hasBackStack);
 
         return true;
     }
 
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
         switch (item.getItemId()) {
             case android.R.id.home:
                 mFragmentManager.popBackStack();
@@ -147,20 +204,54 @@ public class DocumentBrowserActivity extends Activity implements
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * Add the initial Fragment with given path.
-     */
-    private void addFragment() {
-        DocumentListFragment fragment = DocumentListFragment.newInstance(mProvider.getId(), mCurrentDocumentId);
-        mFragmentManager.beginTransaction()
-                .add(android.R.id.content, fragment).commit();
+    private void populateProviderIndices() {
+        mProviderIndices = new HashMap<DocumentsProvider, Integer>();
+        for (int pos = 0; pos < mDrawerAdapter.getCount(); ++pos) {
+            mProviderIndices.put(mDrawerAdapter.getItem(pos), pos);
+        }
     }
 
-    private void replaceFragment(String providerId, String documentId) {
-        mCurrentDocumentId = documentId;
-        DocumentListFragment fragment = DocumentListFragment.newInstance(providerId, documentId);
+    private RootCursor getRoot(DocumentsProvider provider) {
+        try {
+            RootCursor c = new RootCursor(provider.queryRoots(null));
+            c.moveToFirst();
+            return c;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            finish();
+            return null;
+        }
+    }
+
+    private void selectItem(int position) {
+        DocumentsProvider provider = (DocumentsProvider)mDrawerList.getItemAtPosition(position);
+        if (provider != mProvider) {
+            replaceFragment(provider, null);
+        }
+
+        // Highlight the selected item, update the title, and close the drawer
+        mDrawerList.setItemChecked(position, true);
+        mDrawerLayout.closeDrawer(mDrawerList);
+    }
+
+    private void changeProvider(DocumentsProvider provider, String documentId) {
+        if (provider != mProvider) {
+            RootCursor c = getRoot(provider);
+            mRootId = c.getDocumentId();
+            setTitle(c.getTitle());
+            c.close();
+            mProvider = provider;
+            mCurrentDocumentId = (documentId != null) ? documentId : mRootId;
+        } else if (documentId != null) {
+            mCurrentDocumentId = documentId;
+        }
+    }
+
+    private void replaceFragment(DocumentsProvider provider, String documentId) {
+        changeProvider(provider, documentId);
+        DocumentListFragment fragment = DocumentListFragment.newInstance(provider.getId(), mCurrentDocumentId);
         mFragmentManager.beginTransaction()
-                .replace(android.R.id.content, fragment)
+                .replace(R.id.content_frame, fragment)
                 .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
                 .addToBackStack(mCurrentDocumentId).commit();
     }
@@ -185,8 +276,7 @@ public class DocumentBrowserActivity extends Activity implements
                     Toast.LENGTH_SHORT).show();
         } else {
             if (document.isDirectory()) {
-
-                replaceFragment(mProvider.getId(), document.getId());
+                replaceFragment(mProvider, document.getId());
             } else {
                 finishWithResult(document);
             }
@@ -208,4 +298,12 @@ public class DocumentBrowserActivity extends Activity implements
     private void unregisterStorageListener() {
         unregisterReceiver(mStorageListener);
     }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
 }
