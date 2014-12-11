f2620784e2b3105a1e495729b4d53e82aacccde7
Hannes Widmoser <widmoser@gmail.com>
2014-12-11 17:38:01 +0100

    added all document fields to document cursor

--- library/src/main/java/com/mobilejazz/coltrane/library/utils/DocumentCursor.java
+++ library/src/main/java/com/mobilejazz/coltrane/library/utils/DocumentCursor.java
@@ -10,12 +10,22 @@ public class DocumentCursor extends CursorWrapper {
     private int mId;
     private int mMimeType;
     private int mName;
+    private int mSummary;
+    private int mLastModified;
+    private int mIcon;
+    private int mFlags;
+    private int mSize;
 
     public DocumentCursor(Cursor c) {
         super(c);
         mId = c.getColumnIndex(DocumentsContract.Document.COLUMN_DOCUMENT_ID);
         mMimeType = c.getColumnIndex(DocumentsContract.Document.COLUMN_MIME_TYPE);
         mName = c.getColumnIndex(DocumentsContract.Document.COLUMN_DISPLAY_NAME);
+        mSummary = c.getColumnIndex(DocumentsContract.Document.COLUMN_SUMMARY);
+        mLastModified = c.getColumnIndex(DocumentsContract.Document.COLUMN_LAST_MODIFIED);
+        mIcon = c.getColumnIndex(DocumentsContract.Document.COLUMN_ICON);
+        mFlags = c.getColumnIndex(DocumentsContract.Document.COLUMN_FLAGS);
+        mSize = c.getColumnIndex(DocumentsContract.Document.COLUMN_SIZE);
     }
 
     public String getId() {
@@ -28,6 +38,26 @@ public class DocumentCursor extends CursorWrapper {
 
     public String getName() {
         return getWrappedCursor().getString(mName);
+    }
+
+    public String getSummary() {
+        return getWrappedCursor().getString(mSummary);
+    }
+
+    public long getLastModified() {
+        return getWrappedCursor().getLong(mLastModified);
+    }
+
+    public int getIcon() {
+        return getWrappedCursor().getInt(mLastModified);
+    }
+
+    public int getFlags() {
+        return getWrappedCursor().getInt(mLastModified);
+    }
+
+    public long getSize() {
+        return getWrappedCursor().getLong(mSize);
     }
 
     public boolean isDirectory() {
