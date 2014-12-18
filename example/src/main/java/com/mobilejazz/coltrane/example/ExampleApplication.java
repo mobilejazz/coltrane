19b1e6937f22ca456740890f98c592fd464c87f1
Hannes Widmoser <widmoser@gmail.com>
2014-12-18 15:03:51 +0100

    [example] added gdrive provider

--- example/src/main/java/com/mobilejazz/coltrane/example/ExampleApplication.java
+++ example/src/main/java/com/mobilejazz/coltrane/example/ExampleApplication.java
@@ -3,6 +3,7 @@ package com.mobilejazz.coltrane.example;
 import android.app.Application;
 
 import com.mobilejazz.coltrane.provider.filesystem.FileSystemProvider;
+import com.mobilejazz.coltrane.provider.gdrive.GoogleDriveProvider;
 
 public class ExampleApplication extends Application {
 
@@ -10,5 +11,6 @@ public class ExampleApplication extends Application {
     public void onCreate() {
         super.onCreate();
         FileSystemProvider.register(getApplicationContext());
+        GoogleDriveProvider.register(getApplicationContext());
     }
 }
